fa1e6e7a6fda6d4317272a56d3d70f5c987b2c2c
Matas Vaitkevičius <vaitkevicius.matas@gmail.com>
2019-07-02 17:22:51 +0300

    Added jwtToken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
   check,
   validationResult
 } = require('express-validator');
 
 const User = require('../../models/User');
-const bcrypt = require('bcryptjs');
+
 
 // @route POST api/users
 // @desc  Register user
@@ -70,13 +73,28 @@ router.post('/', [
       await user.save();
 
       //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
 
-      res.send('User registered')
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'), {
+          expiresIn: 360000
+        }, (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
-
-  });
+  }
+);
 
 module.exports = router;
